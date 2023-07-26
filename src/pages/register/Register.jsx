c5d7156fa492a3a29384d0636cd9bba59798c4dd
Ali <alidrl26@gmail.com>
2023-07-26 15:20:43 +0200

    first commit

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import register from "../../assets/const.png"
+import FlexDiv from '../../styles/Flex'
+const Register = () => {
+  return (
+    <FlexDiv>
+      <img src={register} alt="" />
+    </FlexDiv>
+  )
+}
+
+export default Register
