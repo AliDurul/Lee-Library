974bdac356ee5c430c445635b1a03ead8240cdde
selman <97898216+Selman-S@users.noreply.github.com>
2023-07-28 19:24:43 +0000

    font family update

--- src/styles/Global.styles.jsx
+++ src/styles/Global.styles.jsx
@@ -8,7 +8,7 @@ export const GlobalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Shantell Sans', cursive;
+        font-family: 'Shantell Sans', cursive, roboto,Open Sans;
     }
 
     body{
